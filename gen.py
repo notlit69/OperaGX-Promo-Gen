6e1f03de66bd7997d923077e890289acdc9e5ae6
notlit69 <125183973+notlit69@users.noreply.github.com>
2024-01-24 20:00:22 +0530

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,21 +1,13 @@
-import os
+import tls_client
 import uuid
 import ctypes
 
 from concurrent.futures import ThreadPoolExecutor
+from colorama import Fore, Style,init
 from datetime import datetime
 from threading import Lock
 from traceback import print_exc
 from random import choice
-try:
-    import tls_client
-    from colorama import Fore, Style , init
-except ModuleNotFoundError:
-    os.system("pip install tls_client")
-    os.system("pip install colorama")
-    os.system("cls")
-    import tls_client
-    from colorama import Fore, Style , init
 
 lock = Lock()
 genned = 0
@@ -51,11 +43,35 @@ class O:
         global genned
         response = self.p('https://api.discord.gx.games/v1/direct-fulfillment', json={
             'partnerUserId': str(uuid.uuid4()),
-        },proxy=self.proxy)
+        },proxy=self.proxy,headers={
+    'authority': 'api.discord.gx.games',
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'content-type': 'application/json',
+    'origin': 'https://www.opera.com',
+    'referer': 'https://www.opera.com/',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Opera GX";v="106"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 OPR/106.0.0.0',
+})
         if response.status_code == 429:
             Logger.Sprint("RATELIMIT","You are being rate limited!",Fore.LIGHTYELLOW_EX)
             return
-        ptoken = response.json()['token']
+        if "Gateway Time-out" in response.text:
+            Logger.Sprint("FAILED","Failed To Do Promo Request : Gateway Timeout!",Fore.LIGHTYELLOW_EX)
+            return
+        if "502 Bad Gateway" in response.text:
+            Logger.Sprint("FAILED","Failed To Do Promo Request : Bad Gateway!",Fore.LIGHTYELLOW_EX)
+            return
+        try: 
+            ptoken = response.json()['token']
+        except:
+            open('diag.txt','w').write(response.text)
+            return
         link = f"https://discord.com/billing/partner-promotions/1180231712274387115/{ptoken}"
         Logger.Sprint(f"PROMO",link,Fore.LIGHTGREEN_EX)
         genned += 1
@@ -77,4 +93,4 @@ if __name__=="__main__":
     t = int(Logger.Ask("ENTER","Enter Amount of Threads -> ",Fore.LIGHTBLUE_EX))
     with ThreadPoolExecutor(max_workers=t+1) as exc:
         for i in range(t):
-            exc.submit(gnr)
+            exc.submit(gnr)      
